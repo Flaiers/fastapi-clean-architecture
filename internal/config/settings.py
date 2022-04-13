45fd0af0700e098502d3bdb24cc915f04e057958
Max <bigin.maks@gmail.com>
2022-04-13 19:01:08 +0300

    version

--- internal/config/settings.py
+++ internal/config/settings.py
@@ -14,7 +14,7 @@ class Settings(BaseSettings):
 
     PROJECT_NAME: str = 'FastAPI'
     DESCRIPTION: str = 'FastAPI clean architecture'
-    VERSION: str = '0.6'
+    VERSION: str = '0.8.0'
 
     SWAGGER_UI_PARAMETERS: Dict[str, Any] = {
         'displayRequestDuration': True,
