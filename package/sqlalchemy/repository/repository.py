f101a5cde8a6780c16f836a3945cedb9fc4f5f5b
Max <bigin.maks@gmail.com>
2022-06-04 20:51:43 +0300

    abstract repository

--- package/sqlalchemy/repository/repository.py
+++ package/sqlalchemy/repository/repository.py
@@ -1,17 +1,9 @@
-from typing import (
-    Any,
-    Callable,
-    Dict,
-    Generic,
-    Sequence,
-    Type,
-    TypeVar,
-)
+from typing import Any, Callable, Dict, Generic, Sequence, TypeVar
 
 import sqlalchemy as sa
 from fastapi import Depends, params
 from multimethod import multimethod as overload
-from sqlalchemy.engine import Result
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.declarative import (
     DeclarativeMeta,
@@ -19,14 +11,13 @@ from sqlalchemy.ext.declarative import (
 )
 
 from package.sqlalchemy import get_session
+from package.sqlalchemy.repository import AbstractRepository
 
 Base: DeclarativeMeta = declarative_base()
 Model = TypeVar('Model', bound=Base)
 
 
-class Repository(Generic[Model]):
-
-    model: Type[Model]
+class Repository(AbstractRepository, Generic[Model]):
 
     def __init__(
         self, session: AsyncSession = Depends(get_session),
@@ -57,18 +48,36 @@ class Repository(Generic[Model]):
     def get_pk(self, instance: Model) -> Dict[str, Any] | Any:
         primary_keys = {}
         for pk in self.primary_keys:
-            field = getattr(instance, pk.name)
-            if field is not None:
-                primary_keys[pk.name] = field
+            attr = getattr(instance, pk.name)
+            if attr is not None:
+                primary_keys[pk.name] = attr
 
         if len(primary_keys) > 1:
             return primary_keys
 
         return next(iter(primary_keys.values()))
 
-    async def find(self, *where, **attrs) -> Result:
+    async def find(self, *where, **attrs) -> Model:
         statement = sa.select(self.model).where(*where).filter_by(**attrs)
-        return await self.session.execute(statement)
+        query_result = await self.session.execute(statement)
+        return query_result.unique().scalars().all()
+
+    async def find_one(self, *where, **attrs) -> Model:
+        statement = sa.select(self.model).where(*where).filter_by(**attrs)
+        query_result = await self.session.execute(statement)
+        return query_result.unique().scalar()
+
+    async def find_one_or_none(self, *where, **attrs) -> Model:
+        statement = sa.select(self.model).where(*where).filter_by(**attrs)
+        query_result = await self.session.execute(statement)
+        return query_result.unique().scalar_one_or_none()
+
+    async def find_one_or_fail(self, *where, **attrs) -> Model:
+        instance = await self.find_one_or_none(*where, **attrs)
+        if instance is None:
+            raise NoResultFound('{0} not found'.format(self.model))
+
+        return instance
 
     @overload
     async def delete(self, *where, **attrs) -> None:
@@ -77,25 +86,40 @@ class Repository(Generic[Model]):
         await self.session.commit()
 
     @overload
-    async def delete(self, instances: Sequence[Model]) -> None:
-        for instance in instances:
-            await self.session.delete(instance)
-        await self.session.commit()
-
-    @overload
     async def delete(self, instance: Model) -> None:
         await self.session.delete(instance)
         await self.session.commit()
 
+    @overload
+    async def delete(self, instances: Sequence[Model]) -> None:
+        for instance in instances:
+            await self.session.delete(instance)
+
+        await self.session.commit()
+
+    @overload
     async def pre_save(self, instance: Model) -> Model:
         self.session.add(instance)
         await self.session.flush()
         return instance
 
+    @overload
+    async def pre_save(self, instances: Sequence[Model]) -> Model:
+        self.session.add_all(instances)
+        await self.session.flush()
+        return instances
+
+    @overload
     async def save(self, instance: Model) -> Model:
         await self.pre_save(instance)
         await self.session.commit()
         return instance
+
+    @overload
+    async def save(self, instances: Sequence[Model]) -> Model:
+        await self.pre_save(instances)
+        await self.session.commit()
+        return instances
 
 
 def Inject(  # noqa: N802
